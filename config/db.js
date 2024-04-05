8ad86dfd2693d3f752cdcd811ab1d7add73521a7
akplmathan <akplmathan@gmail.com>
2024-04-05 11:18:57 +0530

    Initial Commit

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const URL='mongodb+srv://akplmathan:Mathan2003@cluster0.kz3flho.mongodb.net/Project-1'
+require('dotenv').config()
 
 const connectDB = async()=>{
        try {
-            await mongoose.connect(URL);
+            await mongoose.connect(process.env.URL);
             console.log("mongodb connected SuccessFully")
        } catch (error) {
         console.log(error)
